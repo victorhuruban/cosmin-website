db0e4419812ac9ee0ac6c509a2fc934ed2e92012
Victor Huruban <victor.huruban@yahoo.com>
2020-06-30 20:25:58 +0300

    Added 'Go to top' button; still needs visual work

---
+++ script.js
@@ -0,0 +1,16 @@
+mybutton = document.getElementById("topButton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
